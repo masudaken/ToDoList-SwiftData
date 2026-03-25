2c6cb7bb8e0134abac5db0e0e19b3e3be727ac7d
Ken Masuda <masudaken@gmail.com>
2026-03-25 16:44:06 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 増田研 on 2026/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
